11abd40acea79a021a7abfd88a113154526d940e
Jasprit45 <singhsardarjasprit@gmail.com>
2026-03-24 10:07:06 +0530

    Basic sign up page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Login.css'
+import assets from '../../assets/my_assets/assets'
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className='logo' />
+        <form className='login-form' action="">
+            <h2>Sign Up</h2>
+            <input type="text" className="form-input" placeholder='username' required />
+            <input type="email" className="form-input" placeholder='example123@gmail.com...' required />
+            <input type="password" className="form-input" placeholder='password...' required />
+            <button type='submit'>Sign Up</button>
+            <div className="login-term">
+                <input type="checkbox" />
+                <p>Agree to the terms of use & privacy policy.</p>
+            </div>
+            <div className="login-forgot">
+                <p className="login-toggle">Already have an account <span>click here</span></p>
+            </div>
+        </form>
+
+        
+    </div>
   )
 }
 
