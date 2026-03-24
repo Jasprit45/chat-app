5d125e61c4220873433e25e82527b1867dc99595
Jasprit45 <singhsardarjasprit@gmail.com>
2026-03-24 20:36:22 +0530

    Basic chat page UI

---
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './RightSidebar.css'
+import assets from '../../assets/my_assets/assets'
+
+function RightSidebar() {
+  return (
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Richard <img src={assets.green_dot} alt="" className='dot' /> </h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+    </div>
+  )
+}
+
+export default RightSidebar
